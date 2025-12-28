c8f843551e65c68e4f68960c215145cc76d654f4
Nithesh Kumar <nitheshkumar0612@gmail.com>
2025-12-28 21:37:55 +0530

    introducing mmkv for cart persist in zustand

--- src/stateglobal/zustand/product.store.ts
+++ src/stateglobal/zustand/product.store.ts
@@ -1,6 +1,23 @@
+import { createMMKV } from "react-native-mmkv";
 import { create } from "zustand";
+import { createJSONStorage, persist, StateStorage } from "zustand/middleware";
 import { fetchProductsApi } from "./product.api";
 
+export const mmkv = createMMKV({
+  id: "cart-storage",
+});
+
+export const zustandMMKVStorage: StateStorage = {
+  setItem: (name, value) => {
+    mmkv.set(name, value);
+  },
+  getItem: (name) => {
+    return mmkv.getString(name) ?? null;
+  },
+  removeItem: (name) => {
+    mmkv.remove(name);
+  },
+};
 export type CartItem = {
   id: number;
   category: string;
@@ -23,35 +40,40 @@ type TActions = {
   fetchProducts: () => void;
 };
 
-export const useCartProduct = create<TState & TActions>((set) => ({
-  cart: [],
-  products: [],
-  fetchProducts: async () => {
-    const data = await fetchProductsApi();
-    set({ products: data || [] });
-  },
-  addItemToCart: (item) =>
-    set((state) => {
-      const existingItem = state.cart.find((ele) => ele.id === item?.id);
-      if (!existingItem) {
-        return { cart: [...state.cart, { ...item, quantity: 1 }] };
-      }
+export const useCartProduct = create<TState & TActions>()(
+  persist(
+    (set) => ({
+      cart: [],
+      products: [],
+      fetchProducts: async () => {
+        const data = await fetchProductsApi();
+        set({ products: data || [] });
+      },
+      addItemToCart: (item) =>
+        set((state) => {
+          const existingItem = state.cart.find((ele) => ele.id === item?.id);
+          if (!existingItem) {
+            return { cart: [...state.cart, { ...item, quantity: 1 }] };
+          }
 
-      return {
-        cart: state.cart.map((ele) =>
-          ele.id === item.id
-            ? { ...ele, quantity: (ele.quantity || 0) + 1 }
-            : ele
-        ),
-      };
-    }),
+          return {
+            cart: state.cart.map((ele) =>
+              ele.id === item.id
+                ? { ...ele, quantity: (ele.quantity || 0) + 1 }
+                : ele
+            ),
+          };
+        }),
 
-  removeItemFromCart: (id: number) =>
-    set((state) => {
-      const newCart = state.cart?.filter((item) => item.id !== id);
-      return { cart: newCart };
+      removeItemFromCart: (id: number) =>
+        set((state) => {
+          const newCart = state.cart?.filter((item) => item.id !== id);
+          return { cart: newCart };
+        }),
     }),
-}));
+    { name: "product", storage: createJSONStorage(() => zustandMMKVStorage) }
+  )
+);
 
 //SAMPLE BOILERPLATE
 // const storege=create((set)=>({
